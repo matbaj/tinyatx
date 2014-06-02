92dc9ead379ca7b387f9d75ab4584f7c25d3c510
greyfox <greyfox@bshellz.pl>
2014-06-02 14:26:57 +0200

    recognize turned off raspberry pi

--- main.c
+++ main.c
@@ -6,10 +6,26 @@
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 
-uint8_t turned_on = 0;
-uint8_t g=0;
-uint8_t timer = 0;
+uint8_t sine[91] = {0, 1, 2, 3, 5, 8, 11, 15, 20, 25, 30, 36, 43, 49, 56, 64, 72, 80, 88, 97, 105, 114, 123, 132, 141, 150, 158, 167, 175, 183, 191, 199, 206, 212, 219, 225, 230, 235, 240, 244, 247, 250, 252, 253, 254, 255, 254, 253, 252, 250, 247, 244, 240, 235, 230, 225, 219, 212, 206, 199, 191, 183, 175, 167, 158, 150, 141, 132, 123, 114, 105, 97, 88, 80, 72, 64, 56, 49, 43, 36, 30, 25, 20, 15, 11, 8, 5, 3, 2, 1, 0};
+volatile uint8_t turned_on = 0;
+volatile uint8_t g=0;
+volatile uint8_t timer = 0;
+volatile uint8_t shutdown_timeout = 0;
+volatile uint8_t shutdown = 0; /* 0 -nothing, 1- shutdown trigged */
+volatile uint8_t gpio_down = 0; 
 
+void poweroff() 
+{
+  shutdown = 0;
+  OCR0B = 0;
+  g = 0;
+  turned_on = 0;
+  timer = 0;
+  PORTB &= (~(1)<<PB0);
+  shutdown_timeout = 0;
+  gpio_down = 0;
+}
+ 
 
 ISR(WDT_vect)
 {
@@ -18,11 +34,23 @@ ISR(WDT_vect)
   { 
     if(++timer>60)
     {
-      timer = 0;
-      PORTB &= (~(1)<<PB0); 
-       _delay_ms(500);
-      PORTB |= (1<<PB0);
-    } 
+      if(shutdown < 1)
+      {
+        timer = 0;
+        PORTB &= (~(1)<<PB0); 
+        _delay_ms(500);
+        PORTB |= (1<<PB0);
+      } else {
+        poweroff();
+      }
+    }
+    if (shutdown_timeout > 0) shutdown_timeout--;
+    if (!(PINB & (1<<PB2)))
+    {
+      if(gpio_down > 3) poweroff(); /* gpio down too long => raspberry pi is turned off */
+      else gpio_down++;
+    }
+    else gpio_down = 0;
   }
   sei();
   return;
@@ -33,25 +61,23 @@ ISR(PCINT0_vect)
 {
   cli();
 
-  if ((PINB & (1<<PB2)) ^ (1<<PB2))
+  if (!(PINB & (1<<PB2)))
   {
     _delay_ms(200);
     if (turned_on == 1) 
     {
       timer = 0;
+      if (shutdown_timeout > 0) shutdown = 1;
+      else shutdown_timeout = 2;
     }
   }
-  if ((PINB & (1<<PB5))^ (1<<PB5))
+  if (!(PINB & (1<<PB5)))
   {
     _delay_ms(200);
     if (turned_on == 1) 
     {
-      OCR0B = 0;
-      g = 0;
-      turned_on = 0;
-      timer = 0;
-      
-      PORTB &= (~(1)<<PB0);
+      if (shutdown_timeout > 0) poweroff();
+      else shutdown_timeout = 2;
     } 
     else 
     {
@@ -62,12 +88,11 @@ ISR(PCINT0_vect)
   sei();
   return;
 } 
- 
+
+
 int main(void)
 { 
-  
-  uint8_t sine[91] = {0, 1, 2, 3, 5, 8, 11, 15, 20, 25, 30, 36, 43, 49, 56, 64, 72, 80, 88, 97, 105, 114, 123, 132, 141, 150, 158, 167, 175, 183, 191, 199, 206, 212, 219, 225, 230, 235, 240, 244, 247, 250, 252, 253, 254, 255, 254, 253, 252, 250, 247, 244, 240, 235, 230, 225, 219, 212, 206, 199, 191, 183, 175, 167, 158, 150, 141, 132, 123, 114, 105, 97, 88, 80, 72, 64, 56, 49, 43, 36, 30, 25, 20, 15, 11, 8, 5, 3, 2, 1, 0};
-  
+
   PORTB = 0b00100100;
   DDRB  = 0b00000011;
 
